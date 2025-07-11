8309f96c8e87441db988f710faa8c980163cc73e
Muhammed Can Küçükaslan <muhammed.kucukaslan@getir.com>
2025-07-11 14:41:24 +0300

    [refactor]: update dependencies and add patch for abseil compatibility

--- Formula/or-tools.rb
+++ Formula/or-tools.rb
@@ -9,8 +9,27 @@ class OrTools < Formula
   # This formula conflicts with the or-tools formula from homebrew-core
   conflicts_with "or-tools", because: "this is a custom build that replaces the core formula"
 
-  depends_on "protobuf"
+  depends_on "cmake" => [:build, :test]
+  depends_on "pkgconf" => [:build, :test]
   depends_on "abseil"
+  depends_on "cbc"
+  depends_on "cgl"
+  depends_on "clp"
+  depends_on "coinutils"
+  depends_on "eigen"
+  depends_on "openblas"
+  depends_on "osi"
+  depends_on "protobuf"
+  depends_on "re2"
+  depends_on "scip"
+  uses_from_macos "zlib"
+
+  # Add missing `#include`s to fix incompatibility with `abseil` 20240722.0.
+  # https://github.com/google/or-tools/pull/4339
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/bb1af4bcb2ac8b2af4de4411d1ce8a6876ed9c15/or-tools/abseil-vlog-is-on.patch"
+    sha256 "0f8f28e7363a36c6bafb9b60dc6da880b39d5b56d8ead350f27c8cb1e275f6b6"
+  end
 
   def install
     # This appears to be a precompiled binary distribution
