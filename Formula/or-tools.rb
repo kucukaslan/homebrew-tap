6be0a19ef45dff7ed7163ecd3d4435e3dad79584
Muhammed Can Küçükaslan <muhammed.kucukaslan@getir.com>
2025-07-11 14:38:58 +0300

    [refactor]: clean up or-tools formula by removing unnecessary comments and improving descriptions

--- Formula/or-tools.rb
+++ Formula/or-tools.rb
@@ -1,38 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class OrTools < Formula
-  desc "Google's Operations Research tools:"
+  desc "Google's Operations Research tools (custom build)"
   homepage "https://developers.google.com/optimization/"
   url "https://github.com/kucukaslan/or-tools/releases/download/v9.15-alpha15/ortools-arm64-macos-Make-849de053bdf664b2f77f3e936a318715fdd5c1fb.tar.gz"
   sha256 "26f65ed6db56fd6022cee4e1d63dcaf456b231d3b0f49d50c4cd7fc876811f11"
   license "Apache-2.0"
+  version "9.15-alpha15"
 
-  # depends_on "cmake" => :build
+  # This formula conflicts with the or-tools formula from homebrew-core
+  conflicts_with "or-tools", because: "this is a custom build that replaces the core formula"
 
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
+  depends_on "protobuf"
+  depends_on "abseil"
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # This appears to be a precompiled binary distribution
+    # Copy all contents to the prefix
+    prefix.install Dir["*"]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test or-tools`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # Test that the or-tools library can be imported in Python
+    system "python3", "-c", "import ortools; print('OR-Tools imported successfully')"
   end
 end
