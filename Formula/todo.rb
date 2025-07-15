25ec409a3d5591354bc33ba2fe93c52d63ef3ee7
Muhammed Can Küçükaslan <muhammed.kucukaslan@getir.com>
2025-07-16 00:02:42 +0300

    [fix]: update todo formula to correct version and URLs for release 0.1.4-alpha-2

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -3,18 +3,18 @@
 class Todo < Formula
   desc "A simple command-line todo application"
   homepage "https://github.com/kucukaslan/todo"
-  version "0.4.2"
+  version "0.1.4-alpha-2"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_intel do
-      url "https://github.com/kucukaslan/todo/releases/download/v0.4.2/todo_0.4.2_Darwin_x86_64.tar.gz"
-      sha256 "b21e4f9bf2999acdb988ae4e1c319968551cc7a88e249c224a6b479698c50502"
+      url "https://github.com/kucukaslan/todo/releases/download/v0.1.4-alpha-2/todo_0.1.4-alpha-2_Darwin_x86_64.tar.gz"
+      sha256 "ade333eba4ebea4e426d237352cd2f955c79d5a4ec5c7dfb123b88285c39753c"
     end
 
     on_arm do
-      url "https://github.com/kucukaslan/todo/releases/download/v0.4.2/todo_0.4.2_Darwin_arm64.tar.gz"
-      sha256 "ffbeb61eab4ffa561747bee7e4ab5996410aa1cc6d2767139c1c9acaff1203ee"
+      url "https://github.com/kucukaslan/todo/releases/download/v0.1.4-alpha-2/todo_0.1.4-alpha-2_Darwin_arm64.tar.gz"
+      sha256 "2046e265d0895dfd33b6c25a3894e94c83c87c30a51ef38b02d362ed071f8996"
     end
   end
 
